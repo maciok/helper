864e91e2e2ea95a837b5b1e5cabf69c162b32e2b
Maciej Pietrzak <maciek1pietrzak@gmail.com>
2019-11-23 14:04:34 +0100

    init

---
+++ src/test/java/pl/thecode/helper/HelperApplicationIT.java
@@ -0,0 +1,13 @@
+package pl.thecode.helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelperApplicationIT {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
