a1856ec58f1e6908dd5d4e33f553cba2d105a5b0
Maciej Pietrzak <maciek1pietrzak@gmail.com>
2019-11-24 10:24:24 +0100

    add maps

--- src/ui/src/app/dashboard/dashboard.component.ts
+++ src/ui/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {} from 'googlemaps';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  @ViewChild('mapRef', {static: true}) mapElement: ElementRef;
+  map: google.maps.Map;
+
   constructor() { }
 
   ngOnInit() {
+    const mapProperties = {
+      center: new google.maps.LatLng(35.2271, -80.8431),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapProperties);
+
+    var marker = new window['google'].maps.Marker({
+      position: {lat: 35.2271, lng: -80.8431},
+      map: this.map,
+      title: 'Hello World!',
+      draggable: false,
+      animation: window['google'].maps.Animation.DROP,
+    });
   }
 
 }
