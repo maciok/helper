87695b349f8fcd3d5048aa324b80ddef8df5b115
Damian Orzepowski <damian.orzepowski@circlekeurope.com>
2019-11-24 10:53:02 +0100

    notifications on help request

--- src/main/java/pl/thecode/helper/user/UserRepository.java
+++ src/main/java/pl/thecode/helper/user/UserRepository.java
@@ -1,12 +1,12 @@
 package pl.thecode.helper.user;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-interface UserRepository extends CrudRepository<UserEntity, Long> {
+interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     Optional<UserEntity> findByUuid(String uuid);
 }
